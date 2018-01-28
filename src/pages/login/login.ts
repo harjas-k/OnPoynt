2bed70c9e084ef240de21ef3b223c53075e10ce2
Harjas Kaur <harjas.kaur94@gmail.com>
2018-01-27 22:09:54 -0600

    Add files via upload

---
+++ src/pages/login/login.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { HomePage } from '../home/home';
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LogIn {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  pushPage(){
+    this.navCtrl.push(HomePage);
+  }
+
+
+
+}
+
+
