9376bc7eed4063f0882da06d520b6a395137fa95
greeshmanall <nallapareddy.greeshma@gmail.com>
2018-02-15 13:44:15 -0600

    finished sponsor page and sponsor confirmation page

--- src/pages/sponsorconf/sponsorconf.ts
+++ src/pages/sponsorconf/sponsorconf.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HomePage } from '../home/home';
 
 @Component({
   selector: 'page-sponsorconf',
   templateUrl: 'sponsorconf.html'
 })
 export class SponsorConfirmation {
+     home= HomePage;
+
   
 }
